909328dc8ca755079c8f86851b273e511665f009
anthonyangre <anthony.angre@edu.huddinge.se>
2024-11-06 08:41:42 +0100

    commit

--- gymnasiearbete/minesweeper/index.php
+++ gymnasiearbete/minesweeper/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+require_once '../dbhs.php';
 if (!isset($_SESSION['grid'])) {
     resetGame();
 }
@@ -54,9 +54,44 @@ if (isset($_GET['reset'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minesweeper</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <h1>Minesweeper</h1>
+<header> 
+        <div class="menu-container" onclick="toggleMenu(this)">
+            <div class="hamburger-menu">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+
+            <!-- Rubrik som glider in från vänster när knappen klickas -->
+            <div class="menu-title">Navigation</div>
+
+            <!-- Dropdown-menyn som visas vid klick -->
+            <div class="dropdown-menu">
+            <a href="#regler" class="dropdown-item">Regler</a>
+            <a href="#info" class="dropdown-item">Info</a>
+            <a href= "leaderboard.php"class="dropdown-item">Leaderboard </a>
+               
+
+            </div>
+        </div>
+
+        <h3 class="rainbow-text">Välkommen till Minesweeper</h3> <!-- Välkomsttext med regnbågsfärg -->
+
+<!-- Länkar för inloggning och registrering -->
+<div class="header-links">
+ 
+    <a href="../login.php">Login</a>  <a href="../signup.php">Signup</a>  <div class="konto">Konto</div>
+
+   
+    </div>
+
+        
+    </header>
+
+    <div id="head2"> Headerbild</div> 
     <div id="status">Game in progress...</div>
     <div id="game-board"></div>
     <button id="reset-button">Reset</button>
